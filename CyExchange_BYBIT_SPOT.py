8451d999f670bc8190c4b9e4a3e85729db03a36b
cymetric <35925612+cymetric@users.noreply.github.com>
2022-01-27 15:49:53 +0900

    Delete CyExchange_BYBIT_SPOT.py

--- CyExchange_BYBIT_SPOT.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8 이건 주석에만 한글 사용가능하게 해준다. a='한글' <- 이런건 안됨. 가능하면 주석에도 한글 적지마 라인1이나 2에 기록해야 함.
-
-import sys
-
-#print('hello CyExchange_BYBIT_SPOT', a)
-print(sys.getdefaultencoding())
-print(sys.getfilesystemencoding())
-
